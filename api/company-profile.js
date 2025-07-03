a64cf507e21e4aad9927ef11437a6bcf9a3c3c81
iseut99 <dr.sam.lankford@gmail.com>
2025-07-03 11:01:45 -0500

    Update company-profile.js

--- api/company-profile.js
+++ api/company-profile.js
@@ -27,7 +27,7 @@ module.exports = async (req, res) => {
       const prompt = `Write a short, professional company bio for "${companyName}" using this information:\n\n${sourceText}`;
 
       const response = await openai.chat.completions.create({
-        model: "gpt-4",
+        model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: prompt }],
         temperature: 0.7,
       });
